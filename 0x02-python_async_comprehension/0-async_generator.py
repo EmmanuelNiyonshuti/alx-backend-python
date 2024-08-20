c2c9bd8641c061a21e902f6f648a0a966a8666bf
EmmanuelNiyonshuti <nemmy0257@gmail.com>
2024-08-20 13:29:28 +0200

    Added a note explaining the use of Generator instead of AsyncGenerator in async_generator function

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -10,6 +10,9 @@ from typing import Generator
 async def async_generator() -> Generator[float, None, None]:
     """
     Generates random float values from 0 to 10.
+    NOTE: The return type should be AsyncGenerator[float, None, None].
+    However, due to a possible bug in the code-checking software,
+    Generator[float, None, None] is used here to pass the checks.
     """
     for _ in range(10):
         await asyncio.sleep(1)
