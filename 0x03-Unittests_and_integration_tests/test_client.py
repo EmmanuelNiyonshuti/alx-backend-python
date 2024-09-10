a57d2b9c2924d8280e294e95a3424dc6bc03d712
EmmanuelNiyonshuti <nemmy0257@gmail.com>
2024-09-10 13:57:13 +0200

    test case for has_license static method

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -39,7 +39,7 @@ class TestGithubOrgClient(unittest.TestCase):
 
     @patch("client.get_json")
     def test_public_repos(self, mock_get_json):
-        """ Test the public repos method """
+        """ Test the `GithubOrgClient.public_repos` method """
 
         mock_get_json.return_value = [
             {"name": "repo1", "license": {"key": "MIT"}},
@@ -56,3 +56,12 @@ class TestGithubOrgClient(unittest.TestCase):
             self.assertEqual(res, ["repo1", "repo2", "repo3"])
             mock_public_repos_url.assert_called_once()
         mock_get_json.assert_called_once_with(repos_url)
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False)
+    ])
+    def test_has_licence(self, nested_map, license_key, license_value):
+        """ test `GithubOrgClient.has_license` method """
+        res = GithubOrgClient.has_license(nested_map, license_key)
+        self.assertEqual(res, license_value)
